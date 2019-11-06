e807ca24127f28ebaa6310bd451e6453c0033e16
abhikanha <54464268+abhikanha@users.noreply.github.com>
2019-11-06 23:10:51 +0530

    Create sumofdigits.c

---
+++ sumofdigits.c
@@ -0,0 +1,17 @@
+#include<stdio.h>  
+int main()  
+{  
+int sum=0,digit;  
+int n,temp;  
+printf("Enter the number to cal. the sum of digits");   
+scanf("%d",&n);  
+temp=n;  
+while(temp>0)  
+{  
+digit=temp%10;   
+temp/=10;   
+sum+=digit;  
+}  
+printf("\n Sum of digits of %d = %d \n",n,sum);  
+return 0;  
+}  
