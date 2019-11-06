1d30215f7193fa6146cf9d9b8b447989e0f7bc6a
abhikanha <54464268+abhikanha@users.noreply.github.com>
2019-11-06 23:02:35 +0530

    Create tableof2.c

---
+++ tableof2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>  
+int main()  
+{  
+int i,a=2,b=1;  
+printf("The table of 2 :");  
+printf("\n ============ \n");  
+for(i=1;i<=10;i++)  
+{  
+b=2*i;  
+printf("%d * %d  = %d \n",a,i,b);  
+}  
+return 0;  
+}  
